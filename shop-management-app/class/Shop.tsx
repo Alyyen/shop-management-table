9545424698d4ef5fc499adcd6cc6701046b37135
Alyssia Colomar <alyssia.colomar@gmail.com>
2025-09-10 21:47:09 +0200

    feat(shop): create a Shop class + create array of datas from the json

---
+++ shop-management-app/class/Shop.tsx
@@ -0,0 +1,20 @@
+export class Shop {
+  constructor(
+    public id: string,
+    public name: string,
+    public children: Shop[] = [],
+    public manager_access: boolean = false,
+    public admin_access: boolean = false
+  ) {}
+
+  static fromJSON(json: any): Shop {
+    let children: Shop[] = [];
+
+    // call the function again for each children
+    json.children.forEach((element: Shop) => {
+      children.push(Shop.fromJSON(element));
+    });
+
+    return new Shop(json.id, json.name, children);
+  }
+}
